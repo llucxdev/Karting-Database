aa7d43cdd61cebd3c8f752820026b6740b08a7ba
Lluc Puertes <puertesarrue@gmail.com>
2024-04-26 18:59:23 +0200

    Creating repository
    Signed-off-by: Lluc Puertes <puertesarrue@gmail.com>

---
+++ src/main/java/com/hibernate/dao/DriverDAO.java
@@ -0,0 +1,43 @@
+package com.hibernate.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.hibernate.model.Driver;
+import com.hibernate.util.HibernateUtil;
+
+public class DriverDAO {
+
+	public static Driver selectDriver(int id) {
+		Transaction transaction = null;
+		Driver driver = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			transaction = session.beginTransaction();
+			driver = session.get(Driver.class, id);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+		}
+		return driver;
+	}
+	
+	public static List<Driver> selectAllSeries() {
+		Transaction transaction = null;
+		List<Driver> driverList = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			transaction = session.beginTransaction();
+			driverList = session.createQuery("FROM Driver", Driver.class).getResultList();
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+		}
+		return driverList;
+		
+	}
+}
---
+++ src/main/java/com/hibernate/model/Driver.java
@@ -0,0 +1,23 @@
+package com.hibernate.model;
+
+import java.sql.Blob;
+import java.time.LocalDate;
+
+public class Driver {
+	int driver_id;
+	String name;
+	String last_name;
+	LocalDate birthdate;
+	String nationality;
+	int number;
+	int races;
+	int podiums;
+	int wins;
+	int fast_laps;
+	int poles;
+	int dnf;
+	int points;
+	//int team;
+	//int points;
+	//Blob img;
+}
